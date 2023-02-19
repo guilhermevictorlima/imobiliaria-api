6c77e2be88d814c50ca842f2536d34925d28278d
Guilherme Lima <guilhermevictorrocha@gmail.com>
2023-02-19 15:59:17 -0300

    Initial commit

---
+++ src/test/java/com/github/zsguil/imobiliariaapi/ImobiliariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zsguil.imobiliariaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobiliariaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
